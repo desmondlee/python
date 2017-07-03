f9f63b09f567fb8ac406e8d6a1c0b5d07f57c8fc
lizhenguo <lizhenguo@xianjincard.com>
2017-07-03 10:54:12 +0800

    input

--- input.py
+++ input.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
 
 #接收用户输入信息，可通过input()实现：
-while(1):
-    name = input("enter your name: ")
-    print(name)
-
+#input接收的都是str类型，可以根据需要转换，如int()转换成数字：
+while(True):
+    s = input('birth: ')
+    birth = int(s)
+    if birth < 2000:
+        print('00前')
+    else:
+        print('00后')
